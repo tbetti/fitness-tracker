536229e7d0338a1b64929457806fff65b38811df
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-11 09:07:50 -0400

    Created server.js; connected to localhost:3000

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+// Import packages
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+// Connect to packages
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(morgan("dev"));
+
+// Set middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Connect to mongoose
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/db", 
+    {useNewUrlParser: true}
+);
+
+// Connect to port
+app.listen(PORT, ()=>{
+    console.log(`Listening at http://localhost:${PORT}`)
+});
