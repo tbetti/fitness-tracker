0869824e916696194bfa8dcd6b500197c119836f
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-11 11:36:49 -0400

    Planned out routers for workoutRoutes.js

--- Develop/routes/workoutRoutes.js
+++ Develop/routes/workoutRoutes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
 const { Workout, Exercise } = require('../models');
 
+// // Get all workouts
+// router.get('/');
+
+// // Create a new workout
+// router.post('/');
+
+// // Edit a workout
+// router.put('/:id');
+
+// // Get a range of workouts
+// router.get('/range');
+
 module.exports = router;
