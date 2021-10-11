6e731cd04ad6997d47f7e21a55c73ef216573be3
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-11 10:37:52 -0400

    Added schema for Exercise model; connected model and schema

--- Develop/models/Exercise.js
+++ Develop/models/Exercise.js
@@ -1,2 +1,41 @@
+const Schema = require('mongoose').Schema;
 
-module.exports = { Exercise };
+// Define schema
+const ExcerciseSchema = new Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true, 
+        min: 0
+    },
+    // The following 3 categories are for 'resistance' type exercises only
+    weight: {
+        type: Number,
+        min: 0
+    },
+    reps: {
+        type: Number,
+        min: 0
+    },
+    sets: {
+        type: Number,
+        min: 0
+    },
+    // The following category is for 'cardio' type exercises only
+    distance: {
+        type: Number,
+        min: 0
+    }
+})
+
+// Connect the Exercise model to the ExcerciseSchema
+const Exercise = mongoose.model('Exercise', ExcerciseSchema);
+
+module.exports = Exercise;
