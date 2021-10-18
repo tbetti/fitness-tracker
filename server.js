9d36a01100007e71f3f4abe1b3e32d830f17c9b6
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-18 18:52:39 -0400

    Edited mongoose.connect command

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(morgan("dev"));
 
 // Set middleware
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Connect backend and front end
 app.use(express.static('public'));
@@ -20,14 +20,16 @@ app.use(viewRoutes);
 
 // Connect to mongoose
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
     {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
         useFindAndModify: false
     },
 );
 
 // Connect to port
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
 });
