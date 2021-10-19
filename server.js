84f569d8af39fcadd1cbb9cb4b9476be80899b94
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-10-18 22:45:09 -0400

    Redeploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(viewRoutes);
 
 // Connect to mongoose
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    "mongodb+srv://tbetti:password2017@cluster0.wz5fu.mongodb.net/workout?retryWrites=true&w=majority" || "mongodb://localhost/workout",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
